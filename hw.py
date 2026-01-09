c023a9a49f85f41fae3d7cdf919b6e481100324c
Zainab-design4346 <zainabshahzad.4346@gmail.com>
2026-01-09 14:06:05 +0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,11 @@
+set_A = {1, 2, 3, 4, 5}
+set_B = {6, 7, 3, 9, 4}
+result= set_A.symmetric_difference(set_B)
+print("Symmetric Difference:",result)
+print("/n")
+
+set_A = {"zainab", "musa", "ibrahim"}
+set_B = {"zainab", "esha", "fatima"}
+result2= set_A.symmetric_difference(set_B)
+print("Symmetric_difference:",result2)
+print("/n")
